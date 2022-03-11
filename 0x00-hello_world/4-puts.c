0156badf41b392d971167f905fc80af24d009eb4
root <mudicnoni@>
2022-03-11 01:31:21 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * *main - This uses the puts builtin function to print line to standard output
+ *main - This uses the puts builtin function to print line to standard output
  * 
  * Return: 0
  */
