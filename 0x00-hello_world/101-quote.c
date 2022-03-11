ca8ac74d7ab3d057e1994603e92385965a1bcda5
root <mudicnoni@>
2022-03-10 16:30:51 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, 59);
+	return (1);
+}
